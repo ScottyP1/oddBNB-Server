0dd970890c2365a0274efc642852cf30670a0573
Scottyp1 <codymoto122@gmail.com>
2026-02-21 14:05:50 -0500

    feat(models): Added models -> (User, Review, Listing, Favorite, Booking, Amenities)

---
+++ src/main/java/com/oddbnbserver/models/Favorite.java
@@ -0,0 +1,32 @@
+package com.oddbnbserver.models;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "favorites")
+public class Favorite {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "listing_id", nullable = false)
+    private Listing listing;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+
+    public Favorite() {
+    }
+
+    public Favorite(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+}
---
+++ src/main/java/com/oddbnbserver/models/User.java
@@ -0,0 +1,89 @@
+package com.oddbnbserver.models;
+
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    public enum Role {
+        ADMIN,
+        HOST,
+        GUEST
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role = Role.GUEST;
+
+    @OneToMany(mappedBy = "host", fetch = FetchType.LAZY)
+    private List<Listing> hostedListings;
+
+    @OneToMany(mappedBy = "user", fetch = FetchType.LAZY)
+    private List<Favorite> favorites;
+
+    @OneToMany(mappedBy = "author")
+    private List<Review> reviewsWritten;
+    
+    public User() {
+    }
+
+    public User(String firstName, String lastName, String email, Role role) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.role = role;
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+}
