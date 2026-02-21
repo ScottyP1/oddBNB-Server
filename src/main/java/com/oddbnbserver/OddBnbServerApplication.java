e9eb9b5e360c549a862547000b4bf2c657fdcf78
Scottyp1 <codymoto122@gmail.com>
2026-02-20 22:18:59 -0500

    First commit

---
+++ src/main/java/com/oddbnbserver/OddBnbServerApplication.java
@@ -0,0 +1,13 @@
+package com.oddbnbserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OddBnbServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OddBnbServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/oddbnbserver/OddBnbServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oddbnbserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OddBnbServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
